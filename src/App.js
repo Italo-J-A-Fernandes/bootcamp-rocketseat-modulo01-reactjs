86b22871492be75aa03ef59e720ba096ec790614
Italo Fernandes <italojafernandes@hotmail.com>
2020-10-28 04:11:17 -0300

    Conceitos de propriedades

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import Header from './components/Header';
 function App() {
   return( 
     <> 
-      <Header />
-      <Header/> 
+      <Header title="Home Page">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Contats">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header> 
     </>
   );
 };
